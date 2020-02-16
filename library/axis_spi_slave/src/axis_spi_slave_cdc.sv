11365775b87e8c1afd75b767f9e7a3184e56f07d
Niantong Du <kokser.merio@gmail.com>
2020-02-16 11:26:37 +0800

    add axis_spi_slave module

---
+++ library/axis_spi_slave/src/axis_spi_slave_cdc.sv
@@ -0,0 +1,32 @@
+/*
+Copyright (c) 2019 Chengdu JinZhiLi Technology Co., Ltd.
+All rights reserved.
+*/
+
+`timescale 1 ns / 1 ps
+`default_nettype none
+
+(* keep_hierarchy="yes" *)
+module axis_spi_slave_cdc #(parameter C_DATA_WIDTH = 1) (
+    input  wire                    clk ,
+    input  wire [C_DATA_WIDTH-1:0] din ,
+    output wire [C_DATA_WIDTH-1:0] dout
+);
+
+    (* ASYNC_REG="true" *)
+    (* IOB="true" *)
+    reg [C_DATA_WIDTH-1:0] sync0;
+
+    (* ASYNC_REG="true" *)
+    reg [C_DATA_WIDTH-1:0] sync1;
+
+    always @ (posedge clk) begin
+        sync0 <= din;
+        sync1 <= sync0;
+    end
+
+    assign dout = sync1;
+
+endmodule
+
+`default_nettype wire
--- library/axis_spi_slave/tb/tb_axis_spi_slave.sv
+++ library/axis_spi_slave/tb/tb_axis_spi_slave.sv
@@ -1,21 +1,7 @@
-//=============================================================================
-//
-// Copyright (C) 2019 Kele
-//
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-// more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//
-//=============================================================================
+/*
+Copyright (c) 2019 Chengdu JinZhiLi Technology Co., Ltd.
+All rights reserved.
+*/
 
 `timescale 1 ns / 1 ps
 `default_nettype none
@@ -24,64 +10,70 @@ module tb_axis_spi_slave;
 
     parameter SPI_PERIOD = 60;
 
-    logic SCK   ;
-    logic SS_N  ;
-    logic MOSI  ;
-    logic MISO_Z;
+    logic SS_I  = 0;
+    logic SS_O  = 0;
+    logic SS_T  = 0;
+    logic SCK_I = 0;
+    logic SCK_O = 0;
+    logic SCK_T = 0;
+    logic IO0_I = 0; // SI
+    logic IO0_O = 0;
+    logic IO0_T = 0;
+    logic IO1_I = 0; // SO
+    logic IO1_O = 0;
+    logic IO1_T = 0;
 
-    logic aclk   ;
-    logic aresetn;
+    logic aclk    = 0;
+    logic aresetn = 0;
 
-    logic [7:0] m_axis_tdata ;
-    logic       m_axis_tvalid;
-    logic       m_axis_tready;
+    logic [7:0] axis_rx_tdata  = 0;
+    logic       axis_rx_tvalid = 0;
+    logic       axis_rx_tready = 0;
 
-    logic [7:0] s_axis_tdata ;
-    logic       s_axis_tvalid;
-    logic       s_axis_tready;
+    logic [7:0] axis_tx_tdata  = 0;
+    logic       axis_tx_tvalid = 0;
+    logic       axis_tx_tready = 0;
 
-    logic stat_rx_overflow;
-    logic stat_tx_underflow;
 
     reg [7:0] tx_buffer [0:100];
 
     task axis_send (input [7:0] tdata);
         @(posedge aclk);
         // Send byte
-        s_axis_tdata  <= tdata;
-        s_axis_tvalid <= 1'b1;
+        axis_tx_tdata  <= tdata;
+        axis_tx_tvalid <= 1'b1;
         // wait byte is received
         forever begin
             @(posedge aclk);
-            if (s_axis_tvalid && s_axis_tready) break;
+            if (axis_tx_tvalid && axis_tx_tready) break;
         end
         // Reset bus
-        s_axis_tdata  <= 0;
-        s_axis_tvalid <= 0;
+        axis_tx_tdata  <= 0;
+        axis_tx_tvalid <= 0;
     endtask
 
     // Send n byte on SPI bus
     task spi_send(input int nbyte, input [7:0] txd[]);
         // Active slave select pin
-        SS_N = 0;
+        SS_I = 0;
         #(SPI_PERIOD/2);
         // Loop byte
         for (int i = 0; i < nbyte; i++) begin
             // Loop bit
             for (int j = 0; j < 8; j++) begin
                 // Data is pushed at rising edge of SCK
-                SCK = 1;
-                MOSI = txd[i][7-j];
+                SCK_I = 1;
+                IO0_I = txd[i][7-j];
                 #(SPI_PERIOD/2);
                 // Slave should sample data at this time
-                SCK = 0;
+                SCK_I = 0;
                 #(SPI_PERIOD/2);
             end
             $display("%t, SPI Master send: 0x%x", $time, txd[i]);
         end
         // Deactive slave select
-        SS_N = 1;
-        MOSI = 1'bZ;
+        SS_I = 1;
+        IO0_I = 1'bZ;
         // Guard time between words
         #(SPI_PERIOD/2);
     endtask
@@ -99,11 +91,11 @@ module tb_axis_spi_slave;
     reg [2:0] rx_cnt = 0;
     reg [7:0] rx_data = 0;
 
-    always @(negedge SCK, posedge SS_N) begin
-        if (SS_N) begin
+    always @(negedge SCK_I, posedge SS_I) begin
+        if (SS_I) begin
             rx_cnt = 0;
         end else begin
-            rx_data[7-rx_cnt] = MISO_Z;
+            rx_data[7-rx_cnt] = IO1_O;
             if (rx_cnt == 7) begin
                 $display("%t, SPI Master got: 0x%x", $time, rx_data);
             end
@@ -122,9 +114,9 @@ module tb_axis_spi_slave;
     end
 
     initial begin
-        s_axis_tdata = 0;
-        s_axis_tvalid = 0;
-        m_axis_tready = 1;
+        axis_tx_tdata = 0;
+        axis_tx_tvalid = 0;
+        axis_rx_tready = 1;
         wait(aresetn);
 
         axis_send(8'hAA);
@@ -132,9 +124,9 @@ module tb_axis_spi_slave;
     end
 
     initial begin
-        SCK = 0;
-        SS_N = 1;
-        MOSI = 1'bZ;
+        SCK_I = 0;
+        SS_I = 1;
+        IO0_I = 1'bZ;
         wait(aresetn);
         #200;
         for (int i = 0; i < 100; i++) begin
@@ -145,14 +137,14 @@ module tb_axis_spi_slave;
     end
 
     always @ (posedge aclk) begin
-        if (m_axis_tvalid && m_axis_tready) begin
-            $display("%t, RX AXIS got: 0x%x", $time, m_axis_tdata);
+        if (axis_rx_tvalid && axis_rx_tready) begin
+            $display("%t, RX AXIS got: 0x%x", $time, axis_rx_tdata);
         end
     end
 
     always @ (posedge aclk) begin
-        if (s_axis_tvalid && s_axis_tready) begin
-            $display("%t, TX AXIS send: 0x%x", $time, s_axis_tdata);
+        if (axis_tx_tvalid && axis_tx_tready) begin
+            $display("%t, TX AXIS send: 0x%x", $time, axis_tx_tdata);
         end
     end
 
