cde78aea595cee5edc51e2217248e37c3fa493d7
Niantong Du <niantong.du@ericsson.com>
2020-03-10 13:51:58 +0800

    up_bram is using up_wr & up_rd interface

--- library/up_bram/src/up_bram.v
+++ library/up_bram/src/up_bram.v
@@ -10,7 +10,7 @@ module up_bram #(
     parameter C_ADDR_WIDTH = 10,
     parameter C_DATA_WIDTH = 32
 ) (
-    (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME clk, ASSOCIATED_RESET rst, ASSOCIATED_BUSIF UP_RD:UP_WR, FREQ_HZ 100000000, PHASE 0.000" *)
+    (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME clk, ASSOCIATED_RESET rst, ASSOCIATED_BUSIF UP_RD:UP_WR, FREQ_HZ 100000000" *)
     (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 clk CLK" *)
     input  wire                      clk       ,
     (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME rst, POLARITY ACTIVE_HIGH" *)
@@ -18,25 +18,25 @@ module up_bram #(
     input  wire                      rst       ,
     // UP_WR interface
     //================
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_WR addr" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_wr:1.0 UP_WR addr" *)
     input  wire [  C_ADDR_WIDTH-1:0] up_wr_addr,
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_WR be" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_wr:1.0 UP_WR be" *)
     input  wire [C_DATA_WIDTH/8-1:0] up_wr_be  ,
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_WR req" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_wr:1.0 UP_WR req" *)
     input  wire                      up_wr_req ,
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_WR din" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_wr:1.0 UP_WR din" *)
     input  wire [  C_DATA_WIDTH-1:0] up_wr_din ,
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_WR ack" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_wr:1.0 UP_WR ack" *)
     output wire                      up_wr_ack ,
     // UP_RD interface
     //================
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_RD addr" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_rd:1.0 UP_RD addr" *)
     input  wire [  C_ADDR_WIDTH-1:0] up_rd_addr,
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_RD req" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_rd:1.0 UP_RD req" *)
     input  wire                      up_rd_req ,
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_RD dout" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_rd:1.0 UP_RD dout" *)
     output wire [  C_DATA_WIDTH-1:0] up_rd_dout,
-    (* X_INTERFACE_INFO = "jzhl:user:up_ipif:1.0 UP_RD ack" *)
+    (* X_INTERFACE_INFO = "jzhl:user:up_rd:1.0 UP_RD ack" *)
     output wire                      up_rd_ack
 );
 
