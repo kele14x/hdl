c7337fe3d46c6bba7353c27f4f3dd83c88e688ea
Niantong Du <kokser.merio@gmail.com>
2020-03-22 19:15:33 +0800

    new component fmc_slv_if

--- library/fmc_slv_if/src/fmc_slv_if.v
+++ library/fmc_slv_if/src/fmc_slv_if.v
@@ -1,45 +1,55 @@
-`timescale 1 ps / 1 ns
+`timescale 1 ns / 1 ps
 `default_nettype none
 
-module fmc_slv_if (
-    input              clk       ,
-    input              rst       ,
-    // FMC SRAM Interface
-    //===================
-    input  wire [11:0] FMC_A     , // Address bus
-    inout  wire [15:0] FMC_D     , // Write/read data bus
-    input  wire [ 1:0] FMC_NBL   , // Write byte enable
-    input  wire        FMC_NE    , // Chip enable
-    input  wire        FMC_NOE   , // Output enable (read enable)
-    input  wire        FMC_NWE   , // Write enable
-    output wire        FMC_NWAIT ,
+module fmc_slv_if #(
+    parameter C_ADDR_WIDTH = 12,
+    parameter C_DATA_WIDTH = 16
+) (
+    // FMC PSRAM Interface
+    //====================
+    input  wire                      FMC_CLK   , // Clock
+    input  wire [  C_ADDR_WIDTH-1:0] FMC_A     , // Address bus
+    input  wire [  C_DATA_WIDTH-1:0] FMC_D_I   , // Write/read data bus
+    output wire [  C_DATA_WIDTH-1:0] FMC_D_O   ,
+    output wire [  C_DATA_WIDTH-1:0] FMC_D_T   ,
+    input  wire [C_DATA_WIDTH/8-1:0] FMC_NBL   , // Write byte enable
+    input  wire                      FMC_NE    , // Chip enable
+    input  wire                      FMC_NL    , // Address valid
+    input  wire                      FMC_NOE   , // Output enable (read enable)
+    input  wire                      FMC_NWE   , // Write enable
+    output wire                      FMC_NWAIT ,
     // BRAM interface
     //===============
-    output wire        porta_clk ,
-    output wire        porta_rst ,
-    output wire [11:0] porta_addr,
-    output wire        porta_en  ,
-    output wire [15:0] porta_din ,
-    output wire [ 1:0] porta_we
+    output wire                      bram_clk  ,
+    output wire                      bram_rst  ,
+    //
+    output wire [  C_ADDR_WIDTH-1:0] bram_addr ,
+    output wire                      bram_en   ,
+    output wire [  C_DATA_WIDTH-1:0] bram_din  ,
+    output wire [C_DATA_WIDTH/8-1:0] bram_we   ,
+    input  wire [  C_DATA_WIDTH-1:0] bram_dout
 );
 
     fmc_slv_if_top inst (
-        .clk       (clk       ),
-        .rst       (rst       ),
-        .FMC_A     (FMC_A     ),
-        .FMC_D     (FMC_D     ),
-        .FMC_NBL   (FMC_NBL   ),
-        .FMC_NE    (FMC_NE    ),
-        .FMC_NOE   (FMC_NOE   ),
-        .FMC_NWE   (FMC_NWE   ),
-        .FMC_NWAIT (FMC_NWAIT ),
-        .porta_clk (porta_clk ),
-        .porta_rst (porta_rst ),
-        .porta_addr(porta_addr),
-        .porta_en  (porta_en  ),
-        .porta_din (porta_din ),
-        .porta_we  (porta_we  )
+        .FMC_CLK  (FMC_CLK  ),
+        .FMC_A    (FMC_A    ),
+        .FMC_D_I  (FMC_D_I  ),
+        .FMC_D_O  (FMC_D_O  ),
+        .FMC_D_T  (FMC_D_T  ),
+        .FMC_NBL  (FMC_NBL  ),
+        .FMC_NE   (FMC_NE   ),
+        .FMC_NL   (FMC_NL   ),
+        .FMC_NOE  (FMC_NOE  ),
+        .FMC_NWE  (FMC_NWE  ),
+        .FMC_NWAIT(FMC_NWAIT),
+        //
+        .bram_clk (bram_clk ),
+        .bram_rst (bram_rst ),
+        .bram_addr(bram_addr),
+        .bram_en  (bram_en  ),
+        .bram_din (bram_din ),
+        .bram_we  (bram_we  ),
+        .bram_dout(bram_dout)
     );
 
-endmodule : fmc_slv_if
-
+endmodule
