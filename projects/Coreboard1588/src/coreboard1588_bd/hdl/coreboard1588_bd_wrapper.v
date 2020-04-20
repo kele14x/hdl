e5a67a871211459d0ad2027ca07efd66f02f48ca
Niantong Du <kokser.merio@gmail.com>
2020-04-20 23:26:45 +0800

    Coreboard1588: bd is upated, but i think it's duplicate file

--- projects/Coreboard1588/src/coreboard1588_bd/hdl/coreboard1588_bd_wrapper.v
+++ projects/Coreboard1588/src/coreboard1588_bd/hdl/coreboard1588_bd_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
-//Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
-//Date        : Fri Apr 17 10:13:47 2020
-//Host        : kele20vm running 64-bit Ubuntu 18.04.4 LTS
+//Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+//Date        : Mon Apr 20 21:39:43 2020
+//Host        : Kele19t running 64-bit major release  (build 9200)
 //Command     : generate_target coreboard1588_bd_wrapper.bd
 //Design      : coreboard1588_bd_wrapper
 //Purpose     : IP block netlist
