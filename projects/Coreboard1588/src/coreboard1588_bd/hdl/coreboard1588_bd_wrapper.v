53da705a4144e1f462a5bf37d25e3924699f04a8
Michiru Morishita <kokser.merio@gmail.com>
2020-04-21 16:47:16 +0800

    I give up, using the autonamed ip xci files

--- projects/Coreboard1588/src/coreboard1588_bd/hdl/coreboard1588_bd_wrapper.v
+++ projects/Coreboard1588/src/coreboard1588_bd/hdl/coreboard1588_bd_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-//Date        : Tue Apr 21 13:51:40 2020
+//Date        : Tue Apr 21 15:56:24 2020
 //Host        : CN-00002823 running 64-bit major release  (build 9200)
 //Command     : generate_target coreboard1588_bd_wrapper.bd
 //Design      : coreboard1588_bd_wrapper
