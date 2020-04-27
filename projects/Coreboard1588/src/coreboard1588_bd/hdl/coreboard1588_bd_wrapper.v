0e8982c5fd3a6c87e5fafa151feb46a34a1b4b73
Michiru Morishita <kokser.merio@gmail.com>
2020-04-27 10:49:54 +0800

    Coreboard1588: Upgrade to v0.6.2

--- projects/Coreboard1588/src/coreboard1588_bd/hdl/coreboard1588_bd_wrapper.v
+++ projects/Coreboard1588/src/coreboard1588_bd/hdl/coreboard1588_bd_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-//Date        : Sun Apr 26 23:17:47 2020
-//Host        : Kele19t running 64-bit major release  (build 9200)
+//Date        : Mon Apr 27 09:08:36 2020
+//Host        : CN-00002823 running 64-bit major release  (build 9200)
 //Command     : generate_target coreboard1588_bd_wrapper.bd
 //Design      : coreboard1588_bd_wrapper
 //Purpose     : IP block netlist
