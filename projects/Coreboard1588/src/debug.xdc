05f362b73d0e204884a16c721429d6d47f2f5f5b
Michiru Morishita <kokser.merio@gmail.com>
2020-10-20 23:17:29 +0800

    delete useless files

--- projects/Coreboard1588/src/debug.xdc
+++
@@ -1,2 +0,0 @@
-# Copyright (c) 2019 Chengdu JinZhiLi Technology Co., Ltd
-# All rights reserved.
--- projects/Lxi-B/hdl/version.sv
+++
@@ -1,9 +0,0 @@
-/*
-Copyright (c) 2020 Chengdu JinZhiLi Technology Co., Ltd.
-All rights reserved.
-*/
-
-parameter [7:0] VERSION_MAJOR = 8'd0;
-parameter [7:0] VERSION_MINOR = 8'd9;
-parameter [7:0] VERSION_PATCH = 8'd6;
-parameter [7:0] VERSION_TWEAK = 8'd0;
