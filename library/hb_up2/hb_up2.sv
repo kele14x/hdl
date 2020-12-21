89764b510cbbdff216de5e10cf2b7f24f0ffa393
Michiru Morishita <kosker.merio@gmail.com>
2020-12-21 21:32:49 +0800

    add hb_up2 module

---
+++ library/hb_up2/hb_up2.sv
@@ -0,0 +1,94 @@
+//******************************************************************************
+// Copyright (C) 2020  kele14x
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//******************************************************************************
+
+// File: hb_up2.sv
+// Brief: Half band up-sample by 2. Interleaved 2 channels.
+`timescale 1ns / 1ps `default_nettype none
+
+module hb_up2_int2 #(
+    parameter int XIN_WIDTH = 16,
+    parameter int COE_WIDTH = 16,
+    parameter int NUM_UNIQUE_COE = 5,
+    parameter signed [COE_WIDTH-1:0] COE_NUMS[NUM_UNIQUE_COE] = {
+      16'h01dc, 16'hfcdb, 16'h0609, 16'hf3c6, 16'h2847
+    },
+    parameter int YOUT_WIDTH = 16,
+    parameter int SRA_BITS = 15
+) (
+    input var  logic                  clk,
+    input var  logic                  rst,
+    input var  logic [ XIN_WIDTH-1:0] xin,
+    output var logic [YOUT_WIDTH-1:0] yout0,
+    output var logic [YOUT_WIDTH-1:0] yout1,
+    output var logic                  ovf
+);
+
+  localparam int RND = (1 <<< (SRA_BITS - 1));
+
+  logic signed [        XIN_WIDTH-1:0] xin_d[NUM_UNIQUE_COE*4];
+
+  logic signed [          XIN_WIDTH:0] adreg[NUM_UNIQUE_COE];
+  logic signed [XIN_WIDTH+COE_WIDTH:0] mreg [NUM_UNIQUE_COE];
+  logic signed [XIN_WIDTH+COE_WIDTH:0] preg [NUM_UNIQUE_COE];
+
+  // Delay taps, tools can automatically absorb registers into DSP and duplicate
+  // registers if needed
+
+  always_ff @(posedge clk) begin
+    xin_d[0] <= xin;
+    for (int i = 1; i < 100; i++) begin
+      xin_d[i] <= xin_d[i-1];
+    end
+  end
+
+  generate
+    for (genvar i = 0; i < NUM_UNIQUE_COE; i++) begin : g_dsp
+
+      always_ff @(posedge clk) begin
+        adreg[i] <= xin_d[i+1] + xin_d[-3*i+NUM_UNIQUE_COE*4-1];
+        mreg[i]  <= adreg[i] * COE_NUMS[i];
+        preg[i]  <= mreg[i] + ((i < NUM_UNIQUE_COE - 1) ? preg[i+1] : RND);
+      end
+
+    end
+  endgenerate
+
+  always_ff @ (posedge clk) begin
+    yout0 <= xin_d[14];
+    yout1 <= preg[0][YOUT_WIDTH+SRA_BITS-1:SRA_BITS];
+  end
+
+  generate
+    if (YOUT_WIDTH + SRA_BITS >= XIN_WIDTH + COE_WIDTH + 1) begin : g_no_ovf
+
+      // Output is full width, no overflow will happen
+      assign ovf = 'b0;
+
+    end else begin : g_ovf
+
+      always_ff @(posedge clk) begin
+        ovf <= ~(&preg[0][XIN_WIDTH+COE_WIDTH:YOUT_WIDTH+SRA_BITS-1] ||
+                 &(~preg[0][XIN_WIDTH+COE_WIDTH:YOUT_WIDTH+SRA_BITS-1]));
+      end
+
+    end
+  endgenerate
+
+
+endmodule
+
+`default_nettype wire
---
+++ library/hb_up2/tb_hb_up2.sv
@@ -0,0 +1,120 @@
+//******************************************************************************
+// Copyright (C) 2020  kele14x
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//******************************************************************************
+
+// File: tb_hb_up2.sv
+// Brief: Test bench for hb_up2
+
+`default_nettype none
+
+module tb_hb_up2 ();
+
+  localparam int ClkPeriod = 10;
+  localparam int DutLatency = 6;
+  localparam int TestVectorLength = 4096;
+
+  localparam int XinWidth = 16;
+  localparam int CoeWidth = 16;
+  localparam int NumUniqueCoe = 5;
+  localparam signed [CoeWidth-1:0] CoeNums[NumUniqueCoe] = {952, -1609, 3090, -6260, 20622};
+  localparam int YoutWidth = 16;
+  localparam int SraBits = 15;
+
+  logic                clk;
+  logic                rst;
+
+  logic [XinWidth-1:0] xin;
+
+  logic [YoutWidth-1:0] yout0, yout0_ref;
+  logic [YoutWidth-1:0] yout1, yout1_ref;
+  logic ovf, ovf_ref;
+
+  logic [ XinWidth-1:0] xin_mem [    TestVectorLength];
+  logic [YoutWidth-1:0] yout_mem[TestVectorLength * 2];
+  logic                 ovf_mem [TestVectorLength * 2];
+
+  initial begin
+    $readmemh("test_hb_up2_input_xin.txt", xin_mem, 0, TestVectorLength - 1);
+    $readmemh("test_hb_up2_output_yout.txt", yout_mem, 0, TestVectorLength * 2 - 1);
+    $readmemh("test_hb_up2_output_ovf.txt", ovf_mem, 0, TestVectorLength * 2 - 1);
+  end
+
+  always begin
+    clk = 0;
+    #(ClkPeriod / 2);
+    clk = 1;
+    #(ClkPeriod / 2);
+  end
+
+  initial begin
+    rst = 1;
+    #100;
+    rst = 0;
+  end
+
+  initial begin
+    $display("*****************");
+    $display("Simulation start.");
+    wait(rst == 0);
+    xin <= 0;
+    #1000;
+    @(posedge clk);
+    fork
+      begin : p_feed_input
+        for (int i = 0; i < TestVectorLength; i++) begin
+          @(posedge clk);
+          xin <= xin_mem[i];
+          @(posedge clk);
+          xin <= 0;
+        end
+      end
+
+      begin : g_gen_ref
+        repeat (DutLatency) @(posedge clk);
+        for (int i = 0; i < TestVectorLength; i++) begin
+          @(posedge clk);
+          yout0_ref <= (i == 0) ? 0 : yout_mem[2*i-1];
+          yout1_ref <= yout_mem[2*i];
+          ovf_ref <= ((i == 0) ? 0 : ovf_mem[2*i-1]) | ovf_mem[2*i];
+          @(posedge clk);
+          yout0_ref <= 0;
+          yout1_ref <= 0;
+          ovf_ref <= 0;
+        end
+      end
+
+    join
+
+    #1000;
+    $display("Simulation ends.");
+    $finish(2);
+  end
+
+
+  hb_up2_int2 #(
+      .XIN_WIDTH     (XinWidth),
+      .COE_WIDTH     (CoeWidth),
+      .NUM_UNIQUE_COE(NumUniqueCoe),
+      .COE_NUMS      (CoeNums),
+      .YOUT_WIDTH    (YoutWidth),
+      .SRA_BITS      (SraBits)
+  ) DUT (
+      .*
+  );
+
+endmodule
+
+`default_nettype wire
