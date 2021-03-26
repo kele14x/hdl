c9677394fd9dcca6d31eca0477a1bbf61a5e2cca
Michiru Morishita <kokser.merio@gmail.com>
2021-03-26 11:12:40 +0800

    i'm little confused about the implementation... maybe i need it simple

--- library/axi4s_vip/axi4s_if.sv
+++ library/axi4s_vip/axi4s_if.sv
@@ -22,15 +22,15 @@ interface axi4s_if #(
 
   // Blow signals are declared as tri, since multiply driver is need to solve
   // the mux.
-  tri [                    (TDATA_WIDTH == 0 ? 0 : (TDATA_WIDTH-1)):0] tdata;
-  tri [                    (TDEST_WIDTH == 0 ? 0 : (TDEST_WIDTH-1)):0] tdest;
-  tri [                        (TID_WIDTH == 0 ? 0 : (TID_WIDTH-1)):0] tid;
-  tri [(TDATA_WIDTH == 0 || HAS_TKEEP == 0 ? 0 : (TDATA_WIDTH/8-1)):0] tkeep;
-  tri                                                                  tlast;
-  tri                                                                  tvalid;
-  tri [(TDATA_WIDTH == 0 || HAS_TSTRB == 0 ? 0 : (TDATA_WIDTH/8-1)):0] tstrb;
-  tri [                    (TUSER_WIDTH == 0 ? 0 : (TUSER_WIDTH-1)):0] tuser;
-  tri                                                                  tready;
+  logic [                    (TDATA_WIDTH == 0 ? 0 : (TDATA_WIDTH-1)):0] tdata;
+  logic [                    (TDEST_WIDTH == 0 ? 0 : (TDEST_WIDTH-1)):0] tdest;
+  logic [                        (TID_WIDTH == 0 ? 0 : (TID_WIDTH-1)):0] tid;
+  logic [(TDATA_WIDTH == 0 || HAS_TKEEP == 0 ? 0 : (TDATA_WIDTH/8-1)):0] tkeep;
+  logic                                                                  tlast;
+  logic                                                                  tvalid;
+  logic [(TDATA_WIDTH == 0 || HAS_TSTRB == 0 ? 0 : (TDATA_WIDTH/8-1)):0] tstrb;
+  logic [                    (TUSER_WIDTH == 0 ? 0 : (TUSER_WIDTH-1)):0] tuser;
+  logic                                                                  tready;
 
   modport master(
       input aclk, aresetn, aclken, tready,
@@ -45,6 +45,35 @@ interface axi4s_if #(
   modport monitor(
       input aclk, aresetn, aclken, tdata, tdest, tid, tkeep, tlast, tvalid, tstrb, tuser, tready
   );
+
+
+  task automatic reset();
+    tdata <= '0;
+    tdest <= '0;
+    tid <= '0;
+    tkeep <= '0;
+    tlast <= '0;
+    tvalid <= '0;
+    tstrb <= '0;
+    tuser <= '0;
+    tready <= '0;
+  endtask
+
+  task automatic master_send(input int cnt, input logic [TDATA_WIDTH-1:0] data[]);
+    $info("Call of master_send()");
+    wait(aresetn);
+
+    for (int i = 0; i < cnt; i++) begin
+      @(posedge aclk);
+      while (i != 0 && IF.tready == 0) begin
+        @(posedge aclk);
+      end
+      tdata  <= data[i];
+      tvalid <= 1'b1;
+    end
+    @(posedge aclk);
+    reset();
+  endtask
 
 endinterface
 
--- library/axi4s_vip/axi4s_vip.sv
+++ library/axi4s_vip/axi4s_vip.sv
@@ -17,31 +17,15 @@ module axi4s_vip #(
     parameter int TID_WIDTH   = 0,
     parameter int TUSER_WIDTH = 0
 ) (
-    input var                                                                      aclk,
-    input var                                                                      aclken,
-    input var                                                                      aresetn,
+    input var      aclk,
+    input var      aclken,
+    input var      aresetn,
     // Slave Side Interface
     //---------------------
-    input     tri [                    (TDATA_WIDTH == 0 ? 0 : (TDATA_WIDTH-1)):0] s_axis_tdata,
-    input     tri [                    (TDEST_WIDTH == 0 ? 0 : (TDEST_WIDTH-1)):0] s_axis_tdest,
-    input     tri [                        (TID_WIDTH == 0 ? 0 : (TID_WIDTH-1)):0] s_axis_tid,
-    input     tri [(TDATA_WIDTH == 0 || HAS_TKEEP == 0 ? 0 : (TDATA_WIDTH/8-1)):0] s_axis_tkeep,
-    input     tri                                                                  s_axis_tlast,
-    input     tri                                                                  s_axis_tvalid,
-    input     tri [(TDATA_WIDTH == 0 || HAS_TSTRB == 0 ? 0 : (TDATA_WIDTH/8-1)):0] s_axis_tstrb,
-    input     tri [                    (TUSER_WIDTH == 0 ? 0 : (TUSER_WIDTH-1)):0] s_axis_tuser,
-    output    tri                                                                  s_axis_tready,
+    axi4s_if.slave s_axis,
     // Master Side Interface
     //----------------------
-    output    tri [                    (TDATA_WIDTH == 0 ? 0 : (TDATA_WIDTH-1)):0] m_axis_tdata,
-    output    tri [                    (TDEST_WIDTH == 0 ? 0 : (TDEST_WIDTH-1)):0] m_axis_tdest,
-    output    tri [                        (TID_WIDTH == 0 ? 0 : (TID_WIDTH-1)):0] m_axis_tid,
-    output    tri [(TDATA_WIDTH == 0 || HAS_TKEEP == 0 ? 0 : (TDATA_WIDTH/8-1)):0] m_axis_tkeep,
-    output    tri                                                                  m_axis_tlast,
-    output    tri                                                                  m_axis_tvalid,
-    output    tri [(TDATA_WIDTH == 0 || HAS_TSTRB == 0 ? 0 : (TDATA_WIDTH/8-1)):0] m_axis_tstrb,
-    output    tri [                    (TUSER_WIDTH == 0 ? 0 : (TUSER_WIDTH-1)):0] m_axis_tuser,
-    input     tri                                                                  m_axis_tready
+    axi4s_if.master m_axis
 );
 
   axi4s_if #(
@@ -55,7 +39,41 @@ module axi4s_vip #(
       .TDEST_WIDTH(TDEST_WIDTH),
       .TID_WIDTH  (TID_WIDTH),
       .TUSER_WIDTH(TUSER_WIDTH)
-  ) IF (
+  ) mvip_if (
+      .aclk   (aclk),
+      .aclken (aclken),
+      .aresetn(aresetn)
+  );
+
+  axi4s_if #(
+      .HAS_ACLKEN (HAS_ACLKEN),
+      .HAS_ARESETN(HAS_ARESETN),
+      .HAS_TKEEP  (HAS_TKEEP),
+      .HAS_TLAST  (HAS_TLAST),
+      .HAS_TREADY (HAS_TREADY),
+      .HAS_TSTRB  (HAS_TSTRB),
+      .TDATA_WIDTH(TDATA_WIDTH),
+      .TDEST_WIDTH(TDEST_WIDTH),
+      .TID_WIDTH  (TID_WIDTH),
+      .TUSER_WIDTH(TUSER_WIDTH)
+  ) svip_if (
+      .aclk   (aclk),
+      .aclken (aclken),
+      .aresetn(aresetn)
+  );
+
+  axi4s_if #(
+      .HAS_ACLKEN (HAS_ACLKEN),
+      .HAS_ARESETN(HAS_ARESETN),
+      .HAS_TKEEP  (HAS_TKEEP),
+      .HAS_TLAST  (HAS_TLAST),
+      .HAS_TREADY (HAS_TREADY),
+      .HAS_TSTRB  (HAS_TSTRB),
+      .TDATA_WIDTH(TDATA_WIDTH),
+      .TDEST_WIDTH(TDEST_WIDTH),
+      .TID_WIDTH  (TID_WIDTH),
+      .TUSER_WIDTH(TUSER_WIDTH)
+  ) pc_if (
       .aclk   (aclk),
       .aclken (aclken),
       .aresetn(aresetn)
@@ -67,53 +85,43 @@ module axi4s_vip #(
   logic                                                                  is_master = 1;
   logic                                                                  is_slave = 1;
 
-  logic [                    (TDATA_WIDTH == 0 ? 0 : (TDATA_WIDTH-1)):0] tdata_s;
-  logic [                    (TDEST_WIDTH == 0 ? 0 : (TDEST_WIDTH-1)):0] tdest_s;
-  logic [                        (TID_WIDTH == 0 ? 0 : (TID_WIDTH-1)):0] tid_s;
-  logic [(TDATA_WIDTH == 0 || HAS_TKEEP == 0 ? 0 : (TDATA_WIDTH/8-1)):0] tkeep_s;
-  logic                                                                  tlast_s;
-  logic                                                                  tvalid_s;
-  logic [(TDATA_WIDTH == 0 || HAS_TSTRB == 0 ? 0 : (TDATA_WIDTH/8-1)):0] tstrb_s;
-  logic [                    (TUSER_WIDTH == 0 ? 0 : (TUSER_WIDTH-1)):0] tuser_s;
-  logic                                                                  tready_s;
-
   // Slave Side Connection
   //----------------------
 
-  assign IF.tdata = is_slave ? s_axis_tdata : tdata_s;
-  assign IF.tdest = is_slave ? s_axis_tdest : tdest_s;
-  assign IF.tid = is_slave ? s_axis_tid : tid_s;
-  assign IF.tkeep = is_slave ? s_axis_tkeep : tkeep_s;
-  assign IF.tlast = is_slave ? s_axis_tlast : tlast_s;
-  assign IF.tvalid = is_slave ? s_axis_tvalid : tvalid_s;
-  assign IF.tstrb = is_slave ? s_axis_tstrb : tstrb_s;
-  assign IF.tuser = is_slave ? s_axis_tuser : tuser_s;
-  assign s_axis_tready = is_slave ? IF.tready : 1'b1;
+  assign pc_if.tdata = is_slave ? s_axis.tdata : mvip_if.tdata;
+  assign pc_if.tdest = is_slave ? s_axis.tdest : mvip_if.tdest;
+  assign pc_if.tid = is_slave ? s_axis.tid : mvip_if.tid;
+  assign pc_if.tkeep = is_slave ? s_axis.tkeep : mvip_if.tkeep;
+  assign pc_if.tlast = is_slave ? s_axis.tlast : mvip_if.tlast;
+  assign pc_if.tvalid = is_slave ? s_axis.tvalid : mvip_if.tvalid;
+  assign pc_if.tstrb = is_slave ? s_axis.tstrb : mvip_if.tstrb;
+  assign pc_if.tuser = is_slave ? s_axis.tuser : mvip_if.tuser;
+  assign s_axis.tready = is_slave ? pc_if.tready : 1'b1;
 
   // Master Side Connection
   //-----------------------
 
-  assign m_axis_tdata = is_master ? IF.tdata : '0;
-  assign m_axis_tdest = is_master ? IF.tdest : '0;
-  assign m_axis_tid = is_master ? IF.tid : '0;
-  assign m_axis_tkeep = is_master ? IF.tkeep : '0;
-  assign m_axis_tlast = is_master ? IF.tlast : '0;
-  assign m_axis_tvalid = is_master ? IF.tvalid : '0;
-  assign m_axis_tstrb = is_master ? IF.tstrb : '0;
-  assign m_axis_tuser = is_master ? IF.tuser : '0;
-  assign IF.tready = is_master ? m_axis_tready : tready_s;
+  assign m_axis.tdata = is_master ? pc_if.tdata : '0;
+  assign m_axis.tdest = is_master ? pc_if.tdest : '0;
+  assign m_axis.tid = is_master ? pc_if.tid : '0;
+  assign m_axis.tkeep = is_master ? pc_if.tkeep : '0;
+  assign m_axis.tlast = is_master ? pc_if.tlast : '0;
+  assign m_axis.tvalid = is_master ? pc_if.tvalid : '0;
+  assign m_axis.tstrb = is_master ? pc_if.tstrb : '0;
+  assign m_axis.tuser = is_master ? pc_if.tuser : '0;
+  assign pc_if.tready = is_master ? m_axis.tready : svip_if.tready;
 
-    axi4s_vip_master source (
-        .ifa(IF)
-    );
-    
-    axi4s_vip_slave sink (
-        .ifa(IF)
-    );
+  axi4s_vip_master source (
+      .ifa(mvip_if)
+  );
 
-    axi4s_vip_pc pc (
-        .ifa(IF)
-    );
+  axi4s_vip_slave sink (
+      .ifa(svip_if)
+  );
+
+  axi4s_vip_pc pc (
+      .ifa(pc_if)
+  );
 
   initial begin
     $display("Found axi4s_vip at: \"%m\"");
@@ -143,34 +151,6 @@ module axi4s_vip #(
     is_slave  = 0;
   endfunction
 
-  task automatic reset();
-    tdata_s <= '0;
-    tdest_s <= '0;
-    tid_s <= '0;
-    tkeep_s <= '0;
-    tlast_s <= '0;
-    tvalid_s <= '0;
-    tstrb_s <= '0;
-    tuser_s <= '0;
-    tready_s <= '0;
-  endtask
-
-  task automatic master_send(input int cnt, input logic [TDATA_WIDTH-1:0] data[]);
-    $info("Call of master_send()");
-    wait(aresetn);
-
-    for (int i = 0; i < cnt; i++) begin
-      @(posedge aclk);
-      while (i != 0 && IF.tready == 0) begin
-        @(posedge aclk);
-      end
-      tdata_s  <= data[i];
-      tvalid_s <= 1'b1;
-    end
-    @(posedge aclk);
-    reset();
-  endtask
-
 endmodule
 
 module axi4s_vip_master #(
@@ -188,7 +168,7 @@ module axi4s_vip_master #(
   axi4s_if.master ifa
 );
 
-endmodule 
+endmodule
 
 module axi4s_vip_slave #(
     parameter int HAS_ACLKEN  = 0,
@@ -205,7 +185,7 @@ module axi4s_vip_slave #(
   axi4s_if.slave ifa
 );
 
-endmodule 
+endmodule
 
 module axi4s_vip_pc #(
     parameter int HAS_ACLKEN  = 0,
@@ -222,6 +202,6 @@ module axi4s_vip_pc #(
   axi4s_if.monitor ifa
 );
 
-endmodule 
+endmodule
 
 `default_nettype wire
