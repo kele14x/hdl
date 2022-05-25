d3280e752d77ac669cc4686c969ed1fb6d70dc4e
Michiru Morishita <kokser.merio@gmail.com>
2022-05-25 14:08:41 +0800

    cmult: restructure file and foler

--- library/cmult/build.bat
+++
@@ -1,2 +0,0 @@
-call "D:\Xilinx\Vivado\2021.1\settings64.bat"
-vivado -mode batch -source build.tcl -nolog -nojou
--- library/cmult/build.tcl
+++
@@ -1,6 +0,0 @@
-source ../scripts/common.tcl
-
-hdl_prj_create cmult
-hdl_prj_files cmult [list \
- "cmult.sv" ]
-exit 2
