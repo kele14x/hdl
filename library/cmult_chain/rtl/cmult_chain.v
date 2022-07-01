edb9e7b42e10daaee8359f3232e022db8ee34892
Michiru Morishita <kokser.merio@gmail.com>
2022-07-01 17:26:52 +0800

    cmult_chain: changed from SystemVerilog to Verilog

---
+++ library/cmult_chain/rtl/cmult_chain.v
@@ -0,0 +1,107 @@
+// File: cmult_chain.v
+// Brief: Complex Multiplier Chain. This module could be act as a base of
+//        complex FIR filter.
+`timescale 1 ns / 1 ps
+//
+`default_nettype none
+
+module cmult_chain #(
+    parameter integer NUM_TAPS = 8,
+    parameter integer A_WIDTH  = 16,
+    parameter integer B_WIDTH  = 16,
+    parameter integer P_WIDTH  = 16,
+    parameter integer SRA_BITS = 15
+) (
+    input  wire                      clk,
+    input  wire                      rst,
+    //
+    input  wire signed [A_WIDTH-1:0] ar [NUM_TAPS],
+    input  wire signed [A_WIDTH-1:0] ai [NUM_TAPS],
+    //
+    input  wire signed [B_WIDTH-1:0] br [NUM_TAPS],
+    input  wire signed [B_WIDTH-1:0] bi [NUM_TAPS],
+    //
+    output wire signed [P_WIDTH-1:0] pr,
+    output wire signed [P_WIDTH-1:0] pi,
+    // Overflow indicator
+    output wire                      ovf
+);
+
+
+  localparam integer Latency = NUM_TAPS + 4;
+  localparam integer PWidthInt = A_WIDTH + B_WIDTH + 1 + $clog2(NUM_TAPS);
+
+  wire signed [PWidthInt-1:0] pc_s[0:NUM_TAPS];
+  wire signed [PWidthInt-1:0] pr_s[0:NUM_TAPS];
+  wire signed [PWidthInt-1:0] pi_s[0:NUM_TAPS];
+
+  logic ovf_r, ovf_i;
+
+  assign pc_s[0] = (1 << (SRA_BITS - 1));
+  assign pr_s[0] = 0;
+  assign pi_s[0] = 0;
+
+  generate
+    genvar i;
+    for (i = 0; i < NUM_TAPS; i = i + 1) begin : g_stage
+
+      cmult_chain_stage #(
+          .A_WIDTH(A_WIDTH),
+          .B_WIDTH(B_WIDTH),
+          .P_WIDTH(PWidthInt)
+      ) i_core (
+          .clk   (clk),
+          .rst   (rst),
+          //
+          .ar    (ar[i]),
+          .ai    (ai[i]),
+          .br    (br[i]),
+          .bi    (bi[i]),
+          //
+          .pc_in (pc_s[i]),
+          .pr_in (pr_s[i]),
+          .pi_in (pi_s[i]),
+          //
+          .pc_out(pc_s[i+1]),
+          .pr_out(pr_s[i+1]),
+          .pi_out(pi_s[i+1])
+      );
+
+    end
+  endgenerate
+
+  adder #(
+      .A_WIDTH (PWidthInt),
+      .B_WIDTH (PWidthInt),
+      .P_WIDTH (P_WIDTH),
+      .SRA_BITS(SRA_BITS)
+  ) i_adder_r (
+      .clk    (clk),
+      .rst    (rst),
+      .a      (pc_s[NUM_TAPS]),
+      .b      (pr_s[NUM_TAPS]),
+      .add_sub(1'b0),
+      .p      (pr),
+      .ovf    (ovf_r)
+  );
+
+  adder #(
+      .A_WIDTH (PWidthInt),
+      .B_WIDTH (PWidthInt),
+      .P_WIDTH (P_WIDTH),
+      .SRA_BITS(SRA_BITS)
+  ) i_adder_i (
+      .clk    (clk),
+      .rst    (rst),
+      .a      (pc_s[NUM_TAPS]),
+      .b      (pi_s[NUM_TAPS]),
+      .add_sub(1'b0),
+      .p      (pi),
+      .ovf    (ovf_i)
+  );
+
+  assign ovf = ovf_r || ovf_i;
+
+endmodule
+
+`default_nettype wire
