28e78dcdcfda2f7effab9b6c32738e103f453b1d
Michiru Morishita <kokser.merio@gmail.com>
2022-07-11 01:35:21 +0800

    cmult_chain: use sv

---
+++ library/cmult_chain/rtl/cmult_chain_stage.sv
@@ -0,0 +1,83 @@
+// File: cmult_chain_stage.sv
+// Brief: Single DSP stage for Complex Multiplier Chain module.
+`timescale 1 ns / 1 ps
+//
+`default_nettype none
+
+module cmult_chain_stage #(
+    parameter int A_WIDTH = 16,
+    parameter int B_WIDTH = 16,
+    parameter int P_WIDTH = 36
+) (
+    input var                       clk,
+    input var                       rst,
+    //
+    input var  signed [A_WIDTH-1:0] ar,
+    input var  signed [A_WIDTH-1:0] ai,
+    //
+    input var  signed [B_WIDTH-1:0] br,
+    input var  signed [B_WIDTH-1:0] bi,
+    //
+    input var  signed [P_WIDTH-1:0] pc_in,
+    input var  signed [P_WIDTH-1:0] pr_in,
+    input var  signed [P_WIDTH-1:0] pi_in,
+    //
+    output var signed [P_WIDTH-1:0] pc_out,
+    output var signed [P_WIDTH-1:0] pr_out,
+    output var signed [P_WIDTH-1:0] pi_out
+);
+
+
+  localparam int Latency = 3;
+
+
+  logic signed [A_WIDTH-1:0] ar_d, ar_dd;
+  logic signed [A_WIDTH-1:0] ai_d, ai_dd;
+
+  logic signed [B_WIDTH-1:0] br_d;
+  logic signed [B_WIDTH-1:0] bi_d, bi_dd;
+
+  logic signed [A_WIDTH:0] addcommon;
+  logic signed [B_WIDTH:0] addr, addi;
+  logic signed [A_WIDTH+B_WIDTH:0] multc, multr, multi;
+
+  // Delay taps, tools will automatically absorb registers into DSP and
+  // duplicate if needed
+  always_ff @(posedge clk) begin
+    ar_d  <= ar;
+    ar_dd <= ar_d;
+    ai_d  <= ai;
+    ai_dd <= ai_d;
+    br_d  <= br;
+    bi_d  <= bi;
+    bi_dd <= bi_d;
+  end
+
+  // DSP1
+  // Common factor (ar - ai) * bi, shared for the calculations of the real and
+  // imaginary final products
+  always_ff @(posedge clk) begin
+    addcommon <= ar_d - ai_d;
+    multc     <= addcommon * bi_dd;
+    pc_out    <= multc + pc_in;
+  end
+
+  // DSP2
+  // Real product ar * (br - bi) + (ar - ai) * bi = ar * br - ai * bi
+  always_ff @(posedge clk) begin
+    addr   <= br_d - bi_d;
+    multr  <= addr * ar_dd;
+    pr_out <= multr + pr_in;
+  end
+
+  // DSP3
+  // Imaginary product ai * (br + bi) + (ar - ai) * bi = ai * br + ar + bi
+  always_ff @(posedge clk) begin
+    addi   <= br_d + bi_d;
+    multi  <= addi * ai_dd;
+    pi_out <= multi + pi_in;
+  end
+
+endmodule
+
+`default_nettype wire
