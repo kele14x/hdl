75a155f660b10120f4c7f59b4aa1ae8c5b097645
Michiru Morishita <kokser.merio@gmail.com>
2022-07-11 00:56:39 +0800

    cmult: remove traditional testbench

--- library/cmult/rtl/cmult.sv
+++ library/cmult/rtl/cmult.sv
@@ -1,47 +1,47 @@
-// File: cmult.v
+// File: cmult.sv
 // Brief: 3-DSP complex multiplier.
 `timescale 1 ns / 1 ps
 //
 `default_nettype none
 
 module cmult #(
-    parameter integer A_WIDTH  = 16,
-    parameter integer B_WIDTH  = 16,
-    parameter integer P_WIDTH  = 16,
-    parameter integer SRA_BITS = 15
+    parameter int A_WIDTH  = 16,
+    parameter int B_WIDTH  = 16,
+    parameter int P_WIDTH  = 16,
+    parameter int SRA_BITS = 15
 ) (
-    input  wire               clk,
-    input  wire               rst,
+    input var                clk,
+    input var                rst,
     //
-    input  wire [A_WIDTH-1:0] ar,
-    input  wire [A_WIDTH-1:0] ai,
+    input var  [A_WIDTH-1:0] ar,
+    input var  [A_WIDTH-1:0] ai,
     //
-    input  wire [B_WIDTH-1:0] br,
-    input  wire [B_WIDTH-1:0] bi,
+    input var  [B_WIDTH-1:0] br,
+    input var  [B_WIDTH-1:0] bi,
     //
-    output reg  [P_WIDTH-1:0] pr,
-    output reg  [P_WIDTH-1:0] pi,
+    output var [P_WIDTH-1:0] pr,
+    output var [P_WIDTH-1:0] pi,
     // Overflow indicator
-    output reg                ovf
+    output var               ovf
 );
 
 
-  localparam integer Latency = 8;
+  localparam int Latency = 8;
 
-  reg signed [A_WIDTH-1:0] ar_d, ar_dd, ar_ddd, ar_dddd, ar_ddddd;
-  reg signed [A_WIDTH-1:0] ai_d, ai_dd, ai_ddd, ai_dddd, ai_ddddd;
+  logic signed [A_WIDTH-1:0] ar_d, ar_dd, ar_ddd, ar_dddd, ar_ddddd;
+  logic signed [A_WIDTH-1:0] ai_d, ai_dd, ai_ddd, ai_dddd, ai_ddddd;
 
-  reg signed [B_WIDTH-1:0] bi_d, bi_dd, bi_ddd, bi_dddd;
-  reg signed [B_WIDTH-1:0] br_d, br_dd, br_ddd, br_dddd;
+  logic signed [B_WIDTH-1:0] bi_d, bi_dd, bi_ddd, bi_dddd;
+  logic signed [B_WIDTH-1:0] br_d, br_dd, br_ddd, br_dddd;
 
-  reg signed [A_WIDTH:0] addcommon;
-  reg signed [B_WIDTH:0] addr, addi;
-  reg signed [A_WIDTH+B_WIDTH:0] mult0, multr, multi, pr_int, pi_int;
-  reg signed [A_WIDTH+B_WIDTH:0] common, common_d, commonr1, commonr2;
+  logic signed [A_WIDTH:0] addcommon;
+  logic signed [B_WIDTH:0] addr, addi;
+  logic signed [A_WIDTH+B_WIDTH:0] mult0, multr, multi, pr_int, pi_int;
+  logic signed [A_WIDTH+B_WIDTH:0] common, common_d, commonr1, commonr2;
 
   // Delay taps, tools will automatically absorb registers into DSP and
   // duplicate if needed
-  always @(posedge clk) begin
+  always_ff @(posedge clk) begin
     ar_d     <= ar;
     ar_dd    <= ar_d;
     ar_ddd   <= ar_dd;
@@ -68,7 +68,7 @@ module cmult #(
   // DSP1
   // Common factor (ar - ai) * bi, shared for the calculations of the real and
   // imaginary final products
-  always @(posedge clk) begin
+  always_ff @(posedge clk) begin
     addcommon <= ar_d - ai_d;
     mult0     <= addcommon * bi_dd;
     common    <= mult0 + (1 << (SRA_BITS - 1));
@@ -76,7 +76,7 @@ module cmult #(
 
   // DSP2
   // Real product ar * (br - bi) + (ar - ai) * bi = ar * br - ai * bi
-  always @(posedge clk) begin
+  always_ff @(posedge clk) begin
     addr   <= br_dddd - bi_dddd;
     multr  <= addr * ar_ddddd;
     pr_int <= multr + commonr1;
@@ -84,13 +84,13 @@ module cmult #(
 
   // DSP3
   // Imaginary product ai * (br + bi) + (ar - ai) * bi = ai * br + ar + bi
-  always @(posedge clk) begin
+  always_ff @(posedge clk) begin
     addi   <= br_dddd + bi_dddd;
     multi  <= addi * ai_ddddd;
     pi_int <= multi + commonr2;
   end
 
-  always @(posedge clk) begin
+  always_ff @(posedge clk) begin
     pr <= pr_int[P_WIDTH+SRA_BITS-1:SRA_BITS];
     pi <= pi_int[P_WIDTH+SRA_BITS-1:SRA_BITS];
   end
@@ -100,15 +100,21 @@ module cmult #(
 
       // Output is full width, no overflow will happen
       initial begin
-        ovf = 'b0;
+        ovf = '0;
       end
 
     end else begin : g_ovf
 
-      always @(posedge clk) begin
-        ovf <= ~(&pr_int[A_WIDTH+B_WIDTH:P_WIDTH+SRA_BITS-1] ||
-                &(~pr_int[A_WIDTH+B_WIDTH:P_WIDTH+SRA_BITS-1])) || ~(
-                &pi_int[A_WIDTH+B_WIDTH:P_WIDTH+SRA_BITS-1] || &(~pi_int[A_WIDTH+B_WIDTH:P_WIDTH+SRA_BITS-1]));
+      always_ff @(posedge clk) begin
+        if (pr_int[A_WIDTH+B_WIDTH:P_WIDTH+SRA_BITS-1] != '0 &&
+          pr_int[A_WIDTH+B_WIDTH:P_WIDTH+SRA_BITS-1] != '1) begin
+          ovf <= 1'b1;
+        end else if (pi_int[A_WIDTH+B_WIDTH:P_WIDTH+SRA_BITS-1] != '0 &&
+          pi_int[A_WIDTH+B_WIDTH:P_WIDTH+SRA_BITS-1] != '1) begin
+          ovf <= 1'b1;
+        end else begin
+          ovf <= 1'b0;
+        end
       end
 
     end
--- library/cmult/tests/iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module iverilog_dump();
-initial begin
-    $dumpfile("cmult.vcd");
-    $dumpvars(0, cmult);
-end
-endmodule
