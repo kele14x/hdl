53da48b49fda347d0696f83714052bd65bcddc48
Michiru Morishita <kokser.merio@gmail.com>
2022-08-12 01:45:49 +0800

    update ch_fir, remove MAC module

---
+++ library/ch_fir/rtl/ch_fir_stage.sv
@@ -0,0 +1,47 @@
+
+// File: ch_fir_mac.sv
+// Brief: Multiplier for ch_fir module.
+`timescale 1 ns / 1 ps
+//
+`default_nettype none
+
+module ch_fir_stage #(
+    parameter int A_WIDTH = 16,
+    parameter int B_WIDTH = 16,
+    parameter int D_WIDTH = 16,
+    parameter int P_WIDTH = 33
+) (
+    input var                       clk,
+    input var  signed [A_WIDTH-1:0] a,
+    input var  signed [B_WIDTH-1:0] b,
+    input var  signed [D_WIDTH-1:0] d,
+    input var  signed [P_WIDTH-1:0] pin,
+    output var signed [P_WIDTH-1:0] pout
+);
+
+  localparam int ADWidth = (A_WIDTH > D_WIDTH ? A_WIDTH : D_WIDTH) + 1;
+
+  logic signed [A_WIDTH-1:0] ar;
+  logic signed [B_WIDTH-1:0] br;
+  logic signed [B_WIDTH-1:0] brr;
+  logic signed [D_WIDTH-1:0] dr;
+
+  logic signed [ADWidth-1:0] ad;
+  logic signed [P_WIDTH-1:0] mreg;
+  logic signed [P_WIDTH-1:0] preg;
+
+  always @(posedge clk) begin
+    ar   <= a;
+    br   <= b;
+    brr  <= br;
+    dr   <= d;
+    ad   <= a + d;
+    mreg <= ad * b;
+    preg <= mreg + pin;
+  end
+
+  assign pout = preg;
+
+endmodule
+
+`default_nettype wire
