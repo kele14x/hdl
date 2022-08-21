58549a4c4a84ef416ebf50650d04f0e714b0f9fc
Michiru Morishita <kokser.merio@gmail.com>
2022-08-21 09:05:10 +0800

    update nco

--- library/nco/rtl/nco.sv
+++ library/nco/rtl/nco.sv
@@ -5,9 +5,9 @@
 `default_nettype none
 
 module nco #(
-    parameter int PHASE_WIDTH  = 32,
-    parameter int DATA_WIDTH   = 16,
-    parameter bit NEGATIVE_SIN = 0
+    parameter int PHASE_WIDTH   = 32,
+    parameter int PHASE_ENTRIES = 3072,
+    parameter int DATA_WIDTH    = 16
 ) (
     input var                           clk,
     input var                           rst,
@@ -17,32 +17,73 @@ module nco #(
     output var signed [ DATA_WIDTH-1:0] cos,
     output var signed [ DATA_WIDTH-1:0] sin,
     //
-    input var         [PHASE_WIDTH-1:0] config_poff,
-    input var         [PHASE_WIDTH-1:0] config_pinc
+    input var         [PHASE_WIDTH-1:0] ctrl_poff,
+    input var         [PHASE_WIDTH-1:0] ctrl_pinc
 );
+
 
   // Local parameters
   //=================
 
-  localparam integer Latency = 7;
+  localparam int Latency = 3;
 
-  localparam integer LutPhaseWidth = (PHASE_WIDTH > 14) ? 14 : PHASE_WIDTH;
+  // Integer bit width of phase word
+  localparam int PhaseIntegerWidth = $clog2(PHASE_ENTRIES);
+  // Fraction bit width of phase word
+  localparam int PhaseFractionWidth = PHASE_WIDTH - PhaseIntegerWidth;
+  // LUT address with
+  localparam int AddrWidth = PhaseIntegerWidth;
 
+  // Notes:
+  //
+  //     1100_000000  - 2 * PI
+  //     1001_000000  - 3 * PI / 2
+  //     0110_000000  -     PI
+  //     0011_000000  -     PI / 2
+  //     0000_000000  -      0
+  localparam bit [PhaseIntegerWidth-1:0] Phase2Pi = PHASE_ENTRIES;
+  localparam bit [PhaseIntegerWidth-1:0] PhasePi = Phase2Pi / 2;
+  localparam bit [PhaseIntegerWidth-1:0] PhasePi2 = Phase2Pi / 4;
+  localparam bit [PhaseIntegerWidth-1:0] Phase3Pi2 = Phase2Pi - PhasePi2;
+
+  localparam bit [PHASE_WIDTH-1:0] Modulus = Phase2Pi << PhaseFractionWidth;
 
   // Check parameters
   //=================
 
   initial begin
-    assert (2 <= PHASE_WIDTH && PHASE_WIDTH <= 16)
+    assert (2 <= PHASE_WIDTH && PHASE_WIDTH <= 32)
     else begin
-      $error("[%m]: Phase word width (PHASE_WIDTH) must be within the range 2 to 16, got %d.",
+      $error("[%m]: Phase word width (PHASE_WIDTH) must be within the range 2 to 32, got %d.",
              PHASE_WIDTH);
+      #1 $finish;
+    end
+
+    assert (PhaseFractionWidth >= 0)
+    else begin
+      $error("[%m]: Phase word width (PHASE_WIDTH) should be large enough to hold PHASE_ENTRIES");
+      #1 $finish;
+    end
+
+    assert (PHASE_ENTRIES >= 12)
+    else begin
+      $error(
+          "[%m]: Number of phase LUT entries (PHASE_ENTRIES) must be equal or lager than 12, got %d.",
+          PHASE_ENTRIES);
+      #1 $finish;
+    end
+
+    assert (Phase2Pi[PhaseIntegerWidth-1-:2] == 2'b11 && Phase2Pi[PhaseIntegerWidth-3:0] == '0)
+    else begin
+      $error(
+          "[%m]: Number of phase LUT entries (PHASE_ENTRIES) must be as the form 3 * 2^n, got %d.",
+          PHASE_ENTRIES);
       #1 $finish;
     end
 
     assert (3 <= DATA_WIDTH && DATA_WIDTH <= 26)
     else begin
-      $error("[%m]: Data word width (DATA_WIDTH) must be within the range 3 to 26, got %d",
+      $error("[%m]: Data word width (DATA_WIDTH) must be within the range 3 to 26, got %d.",
              DATA_WIDTH);
       #1 $finish;
     end
@@ -52,44 +93,119 @@ module nco #(
   // Signals
   //========
 
-  wire [LutPhaseWidth-1:0] phase_s;
+  logic [PHASE_WIDTH-1:0] phase_accumulator;
+  logic [  PHASE_WIDTH:0] phase_wrapped;
+  logic [  PHASE_WIDTH:0] phase_pre_round;
+  logic [  PHASE_WIDTH:0] phase_round;
+
+  logic [PhaseFractionWidth-1:0] lfsr;
+
+  logic [PhaseIntegerWidth-1:0] phase_int;
+  logic [PhaseIntegerWidth-1:0] phase_cos;
+  logic [PhaseIntegerWidth-1:0] phase_sin;
+
+  logic [AddrWidth-1:0] addr_cos;
+  logic [AddrWidth-1:0] addr_sin;
 
 
   // Main
   //=====
 
-  dds_phase_gen #(
-      .PHASE_WIDTH    (PHASE_WIDTH),
-      .LUT_PHASE_WIDTH(LutPhaseWidth),
-      .INIT_PINC      (INIT_PINC),
-      .INIT_POFF      (INIT_POFF)
-  ) i_phase_gen (
-      .clk        (clk),
-      .rst        (rst),
-      //
-      .sync       (sync),
-      //
-      .phase_out  (phase_s),
-      //
-      .config_poff(config_poff),
-      .config_pinc(config_pinc)
+  // Phase accumulator
+
+  always @(posedge clk) begin
+    if (rst) begin
+      phase_accumulator <= '0;
+    end else if (sync) begin
+      phase_accumulator <= ctrl_poff;
+    end else begin
+      phase_accumulator <= phase_wrapped[PHASE_WIDTH-1:0];
+    end
+  end
+
+  // Phase accumulator increase by ctrl_pinc every tick. It will eventually
+  // exceed 2*Pi (Phase2Pi), and need to be wrapped. Unlike 2^N phase case,
+  // phase in modulus M mode does not wrap naturally. Wrap could be done
+  // by subtract by Phase2Pi.
+  always_comb begin
+    phase_wrapped = phase_accumulator + ctrl_pinc;
+    if (phase_wrapped[PHASE_WIDTH-:3] >= 3'b011) begin
+      phase_wrapped = {(phase_wrapped[PHASE_WIDTH-:3] - 3'b011), phase_wrapped[PHASE_WIDTH-3:0]};
+    end
+  end
+
+  // Random rounding
+
+  lfsr #(
+      .BIT_WIDTH      (PhaseFractionWidth),
+      .INITIAL        ({PhaseFractionWidth{1'b1}}),
+      .POLYNOMIAL     ('b00000000000000001001),
+      .STRUCTURE      ("FIBONACCI"),
+      .GATE_TYPE      ("XOR"),
+      .PARALLEL_OUTPUT(1'b1)
+  ) i_lfsr (
+      .clk (clk),
+      .rst (sync),
+      .en  (1'b1),
+      .load(1'b0),
+      .din ('0),
+      .dout(lfsr)
   );
 
+  always_comb begin
+    phase_pre_round = phase_accumulator + lfsr;
+    if (phase_pre_round[PHASE_WIDTH-:3] >= 3'b011) begin
+      phase_pre_round = {(phase_pre_round[PHASE_WIDTH-:3] - 3'b011), phase_pre_round[PHASE_WIDTH-3:0]};
+    end
+  end
 
-  dds_lut #(
-      .PHASE_WIDTH (LutPhaseWidth),
-      .DATA_WIDTH  (DATA_WIDTH),
-      .NEGATIVE_COS(NEGATIVE_COS),
-      .NEGATIVE_SIN(NEGATIVE_SIN)
+  always_ff @(posedge clk) begin
+    phase_round <= phase_pre_round[PHASE_WIDTH-1-:PhaseIntegerWidth];
+  end
+
+  // Map sine phase to cosine phase
+
+  always_comb begin
+    phase_cos = phase_round;
+  end
+
+  // sin(x) = cos(x - pi/2), when x >= pi/2
+  // sin(x) = cos(x + 3*pi/2), when x < pi/2
+  always_comb begin
+    if (phase_round < PhasePi2) begin
+      phase_sin = phase_round + Phase3Pi2;
+    end else begin
+      phase_sin = phase_round - PhasePi2;
+    end
+  end
+
+  // Use LSBs as LUT address
+
+  always_ff @(posedge clk) begin
+    addr_cos <= phase_cos[AddrWidth-1:0];
+  end
+
+  always_ff @(posedge clk) begin
+    addr_sin <= phase_sin[AddrWidth-1:0];
+  end
+
+  // Phase to Cosine/sine LUT
+
+  nco_lut #(
+      .PHASE_ENTRIES(PHASE_ENTRIES),
+      .DATA_WIDTH   (DATA_WIDTH)
   ) i_lut (
-      .clk    (clk),
-      .rst    (rst),
-      .en     (1'b1),
+      .clk  (clk),
       //
-      .phase  (phase_lut),
+      .rsta (rst),
+      .ena  (1'b1),
+      .addra(addr_cos),
+      .douta(cos),
       //
-      .cos_out(cos_out),
-      .sin_out(sin_out)
+      .rstb (rst),
+      .enb  (1'b1),
+      .addrb(addr_sin),
+      .doutb(sin)
   );
 
 endmodule
---
+++ library/nco/rtl/nco_lut.sv
@@ -0,0 +1,96 @@
+// File: nco_lut.v
+// Brief: The stored in memory part of DDS LUT.
+`timescale 1 ns / 1 ps
+//
+`default_nettype none
+
+module nco_lut #(
+    parameter int PHASE_ENTRIES = 3072,
+    parameter int DATA_WIDTH    = 16
+) (
+    input var                                     clk,
+    //
+    input var                                     rsta,
+    input var                                     ena,
+    input var         [$clog2(PHASE_ENTRIES)-1:0] addra,
+    output var signed [           DATA_WIDTH-1:0] douta,
+    //
+    input var                                     rstb,
+    input var                                     enb,
+    input var         [$clog2(PHASE_ENTRIES)-1:0] addrb,
+    output var signed [           DATA_WIDTH-1:0] doutb
+);
+
+
+  localparam int Latency = 2;
+  localparam int AddrWidth = $clog2(PHASE_ENTRIES);
+  localparam real PI = 3.14159265359;
+
+
+  // The Memory
+  (* ram_style="block" *)
+  logic signed [DATA_WIDTH-1:0] MEM     [2**AddrWidth];
+
+  logic                         ena_d;
+  logic                         enb_d;
+
+  logic                         rsta_d;
+  logic                         rstb_d;
+
+  logic signed [DATA_WIDTH-1:0] douta_s;
+  logic signed [DATA_WIDTH-1:0] doutb_s;
+
+
+  initial begin
+    for (int i = 0; i < PHASE_ENTRIES; i++) begin
+      MEM[i] = (2 ** (DATA_WIDTH - 1) - 2) * $cos(2 * PI * i / PHASE_ENTRIES);
+    end
+  end
+
+
+  // Memory port A
+
+  always @(posedge clk) begin
+    ena_d  <= ena;
+    rsta_d <= rsta;
+  end
+
+  always @(posedge clk) begin
+    if (ena) begin
+      douta_s <= MEM[addra];
+    end
+  end
+
+  always @(posedge clk) begin
+    if (rsta_d) begin
+      douta <= '0;
+    end else if (ena_d) begin
+      douta <= douta_s;
+    end
+  end
+
+
+  // Memory port B
+
+  always @(posedge clk) begin
+    enb_d  <= enb;
+    rstb_d <= rstb;
+  end
+
+  always @(posedge clk) begin
+    if (enb) begin
+      doutb_s <= MEM[addrb];
+    end
+  end
+
+  always @(posedge clk) begin
+    if (rstb_d) begin
+      doutb <= '0;
+    end else if (enb_d) begin
+      doutb <= doutb_s;
+    end
+  end
+
+endmodule
+
+`default_nettype wire
