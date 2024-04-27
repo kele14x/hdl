420ca01a95e55923ac231bcc7e472b74750879e2
Michiru Morishita <kokser.merio@gmail.com>
2024-04-27 19:16:06 +0800

    remove dpd_v50

--- dpd_v50/sysgen/pd_path/put_cval.m
+++
@@ -1,7 +0,0 @@
-function [keys, vals] = put_cval(keys, vals, this_key, this_val)
-
-keys = [keys, this_key];
-vals = [vals, this_val];
-
-
-end
--- dpd_v50/sysgen/pd_path/simulation/std_pwr.m
+++
@@ -1,3 +0,0 @@
-function d = std_pwr(x)
-
-d = sqrt( sum( abs(x).^2 ) / length(x) );
