57a7b29083e8d24dafe641b7da880624f0e09bf0
Michiru Morishita <kokser.merio@gmail.com>
2024-04-27 20:12:55 +0800

    fix delay.sv

--- util/delay.sv
+++ util/delay.sv
@@ -1,24 +1,32 @@
-// File: shift_regs.sv
+// File: delay.sv
 // Brief: Shift registers to delay a signal for specific number of clocks. It
 //        let synthesizer select result automatically.
 `timescale 1 ns / 1 ps
 //
 `default_nettype none
 
-module shift_regs #(
-    parameter bit SIM_INIT   = 1,
-    parameter int DATA_WIDTH = 8,
-    parameter int DEPTH      = 8
+module delay #(
+    parameter bit SIM_INIT = 1,
+    parameter int WIDTH    = 8,
+    parameter int DEPTH    = 8
 ) (
-    input var                   clk,
-    input var                   cen,
+    input var              clk,
+    input var              cen,
     //
-    input var  [DATA_WIDTH-1:0] din,
-    output var [DATA_WIDTH-1:0] dout
+    input var  [WIDTH-1:0] din,
+    output var [WIDTH-1:0] dout
 );
 
+  initial begin
+    assert (WIDTH > 0)
+    else $error("[%m]: WIDTH value (%d) should be large than 0", WIDTH);
+
+    assert (DEPTH >= 0)
+    else $error("[%m]: DEPTH value (%d) should be large or equal than 0", DEPTH);
+  end
+
   generate
-    if (DEPTH == 0) begin : g_zero_depth
+    if (DEPTH == 0) begin : g_no_reg
 
       assign dout = din;
 
